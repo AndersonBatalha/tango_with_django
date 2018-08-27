da5c4294a85043954bb73a2c77cfbd76e1b9c003
AndersonBatalha <andersonpbatalha@gmail.com>
2018-08-27 11:41:16 -0300

    Adicionados os links nas views

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,10 +4,37 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    texto = """
+    
+    <h3>Rango says hey there partner!</h3>
+
+    <br><a href='rango/'>Rango app</a>
+    <br><a href='rango/about/'>About</a>
+    
+    """
+    return HttpResponse(texto)
 
 def rango_index(request):
-    return HttpResponse("Rango app says: 'hey there partner!'")
+    texto = """
+
+    <h3>Rango app says: 'hey there partner!'</h3>
+
+    <br><br><a href='about/'>About</a>
+    <br><br><a href='/'>Voltar</a>
+
+
+    """
+
+    return HttpResponse(texto)
 
 def about(request):
-    return HttpResponse("Rango app says: 'is the about page'")
+    texto = """
+
+    <h3>Rango app says: 'is the about page'</h3>
+
+    <br><br><a href='/'>Index</a>
+    <br><br><a href='/rango'>Voltar</a>
+
+
+    """
+    return HttpResponse(texto)
