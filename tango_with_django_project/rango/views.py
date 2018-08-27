a3f105686e6ba3b53f8f46cde52e429baa928ebe
AndersonBatalha <andersonpbatalha@gmail.com>
2018-08-27 11:34:05 -0300

    Criada view about

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Rango says hey there partner!")
+
+def rango_index(request):
+    return HttpResponse("Rango app says: 'hey there partner!'")
+
+def about(request):
+    return HttpResponse("Rango app says: 'is the about page'")
