716a0ee40789d6c2ba189d72b53fcf07ff44c8c0
AndersonBatalha <andersonpbatalha@gmail.com>
2018-09-04 23:21:51 -0300

    Update settings.py

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -124,9 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+STATIC_ROOT = STATIC_DIR
+#STATICFILES_DIRS = [STATIC_DIR,]
 
 
 # Media files
