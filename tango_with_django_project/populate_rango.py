bc9d914afa35f52fa0a79eb052ebdb28ebc5dc26
AndersonBatalha <andersonpbatalha@gmail.com>
2018-09-15 00:30:16 -0300

    Mudanças no script de população do banco (arquivos csv)

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -1,69 +1,67 @@
-import os
-
-#os.system('rm -rf db.sqlite3')
-#os.system('python manage.py migrate')
-#os.system('python manage.py createsuperuser')
+import os, django, random
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tango_with_django_project.settings")
-
-import django
 django.setup()
+
+os.system('rm -rf db.sqlite3')
+os.system('python manage.py migrate')
+os.system('python manage.py makemigrations')
 
 from rango.models import Category, Page
 
+categorias = ['Esportes', 'Filmes', 'Política', 'Django', 'Python', 'git', 'Carros', 'Notícias', 'Televisão', 'Educação', 'Tecnologia', 'virtualenv', 'Entretenimento', 'Culinária', 'Rádios', 'Linux', 'Software livre', 'Ubuntu', 'Wine', 'Tutoriais', 'Eleições', 'Redes sociais', 'Podcast', 'Economia', 'E-mail', 'Jogos', 'Copa do Mundo', 'Humor', 'Vídeos', 'Ionic', 'Android', 'Blogs', 'Windows', 'Debian', 'Red Hat', 'Java', 'Flask', 'Bottle', 'Typescript', 'Bootstrap', 'CSS', 'HTML', 'PHP', 'Shell script', 'Redes', 'Matemática', 'Segurança da informação', 'Saúde', 'F1']
+
+def gerar_arquivo_category():
+    print ("Criando arquivo categories.csv")
+    arquivo_cat = open('categories.csv', 'w')
+    for categoria in categorias:
+        arquivo_cat.write("%s, %d, %d\n" % (categoria, random.randint(1,250), random.randint(1,250)))
 
 def populate():
+    categories = open('categories.csv').readlines()
 
-    python_pages = [
-    {"title": "Official Python Tutorial",
-        "url":"http://docs.python.org/2/tutorial/", "views": 128, "likes": 64},
-    {"title": "How to Think like a Computer Scientist",
-        "url": "http://www.greenteapress.com/thinkpython/", "views": 128, "likes": 64},
-    {"title": "Learn Python in 10 Minutes",
-        "url": "http://www.korokithakis.net/tutorials/python/", "views": 128, "likes": 64}]
+    print('Populating %d categories...' % len(categories))
+    for category in categories:
+        cat, views, likes = category.rstrip().split(',')
+        c = add_cat(cat, views, likes)
 
-    django_pages = [
-    {"title": "Official Django Tutorial",
-        "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views": 64, "likes": 32},
-    {"title": "Django Rocks",
-        "url": "http://www.djangorocks.com/", "views": 64, "likes": 32},
-    {"title": "How to Tango with Django",
-        "url": "http://www.tangowithdjango.com/", "views": 64, "likes": 32}]
-
-    other_pages = [
-    {"title": "Bottle",
-        "url":"http://bottlepy.org/docs/dev/", "views": 32, "likes": 16},
-    {"title": "Flask",
-        "url": "http://flask.pocoo.org", "views": 32, "likes": 16}]
-
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages}}
-
-    for cat, cat_data in cats.items():
+    pages = open('pages.csv').readlines()
+    print('Populating %d pages...' % len(pages))
+    for page in pages:
+        cat, title, url = page.rstrip().split(',')
+        views = random.randint(1,500)
         c = add_cat(cat)
-        for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
-            c.views = p['views']
-            c.likes = p['likes']
-            c.save()
+        ok = add_page(c, title, url, views)
 
+def show():
+    # Print out the categories we have added.
+    print('Showing pages...')
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
-            print ("- {0}  -  {1}".format(str(c), str(p)))
+            print("- {0} - {1}".format(str(c), str(p)))
+
 
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
-    p.url=url
+    p.url = url
     p.views = views
     p.save()
     return p
 
+
 def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    if not c.views:
+        c.views = views
+    if not c.likes:
+        c.likes = likes
     c.save()
     return c
 
+
+# Start execution here!
 if __name__ == '__main__':
-    print('Starting Rango population script...')
+    print("Starting Rango population script...")
+    gerar_arquivo_category()
     populate()
+    show()
