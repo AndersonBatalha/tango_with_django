96b95cb8694bb489929c90f547e212901e73be23
AndersonBatalha <andersonpbatalha@gmail.com>
2018-09-17 11:11:23 -0300

    Exibir as páginas mais visualizadas

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -8,7 +8,10 @@ def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     print('Categorias: ', category_list, len(category_list))
 
-    context_dict = {'categories': category_list}
+    pages = Page.objects.order_by('-views')[:5]
+    print('Páginas', pages, len(pages))
+
+    context_dict = {'categories': category_list, 'pages': pages}
     return render(request, 'rango/index.html', context=context_dict)
 
 
