46f1b7921082f8ac96c95a4ecd3ee4c69ef9ac41
AndersonBatalha <andersonpbatalha@gmail.com>
2018-09-26 22:49:44 -0300

    view add_category

--- tango_with_django_project/rango/views/add_category.py
+++ tango_with_django_project/rango/views/add_category.py
@@ -5,11 +5,11 @@ from ..forms import CategoryForm
 
 def add_category(request):
     form = CategoryForm()
-    if request.method == 'POST':
+    if request.method == 'POST': # verifica se a requisição é do tipo POST
         form = CategoryForm(request.POST)  # requisição é do tipo POST
         if form.is_valid(): # verifica se o form é válido
             form.save(commit=True) # salva as alterações
-            return index(request) # retorna a página inicial
+            return index(request) # se os dados do formulário estiverem válidos, retorna a página inicial
         else:
-            print (form.errors)
-    return render(request, 'rango/add_category.html', {'form': form})
+            print (form.errors) # não salva o formulário se os dados estiverem incorretos
+    return render(request, 'rango/add_category.html', {'form': form}) # renderiza o template e passa como contexto o formulário
