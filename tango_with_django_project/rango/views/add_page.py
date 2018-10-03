445a296920f29dd81d2889c420bfa10875de86e5
AndersonBatalha <andersonpbatalha@gmail.com>
2018-10-03 17:32:06 -0300

    View e template - add_page

--- tango_with_django_project/rango/views/add_page.py
+++ tango_with_django_project/rango/views/add_page.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 
-from ..views import index
-from ..forms import CategoryForm
+from ..views import show_category
+from ..forms import PageForm
+from ..models import Category
 
-def add_category(request):
-    form = CategoryForm()
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+
+    form = PageForm()
     if request.method == 'POST': # verifica se a requisição é do tipo POST
-        form = CategoryForm(request.POST)  # requisição é do tipo POST
+        form = PageForm(request.POST)  # requisição é do tipo POST
         if form.is_valid(): # verifica se o form é válido
-            form.save(commit=True) # salva as alterações
-            return index(request) # se os dados do formulário estiverem válidos, retorna a página inicial
+            if category: # verifica se existe a categoria
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return show_category(request, category_name_slug) # se os dados do formulário estiverem válidos, retorna a página com as páginas de uma categoria
         else:
             print (form.errors) # não salva o formulário se os dados estiverem incorretos
-    return render(request, 'rango/add_category.html', {'form': form}) # renderiza o template e passa como contexto o formulário
+    return render(request, 'rango/add_page.html', {'form': form, 'category': category}) # renderiza o template e passa como contexto o formulário
