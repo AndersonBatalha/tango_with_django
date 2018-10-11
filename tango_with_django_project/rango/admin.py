d86f5da1adf31c66493a1b48081b721b5024942b
AndersonBatalha <andersonpbatalha@gmail.com>
2018-10-11 11:41:28 -0300

    Adicionado o model UserProfile ao admin

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -3,7 +3,7 @@
 
 from django.contrib import admin
 
-from rango.models import Category, Page
+from .models import Category, Page, UserProfile
 
 # Register your models here.
 
@@ -26,6 +26,6 @@ class CategoryAdmin(admin.ModelAdmin):
         ('Curtidas e visualizações', {'fields': ['views', 'likes']})
     ]
 
-
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
