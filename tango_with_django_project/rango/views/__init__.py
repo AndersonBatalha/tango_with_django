ecd0640be0f6ec378ca6e3cb9f061b4eef878146
AndersonBatalha <andersonpbatalha@gmail.com>
2018-10-14 20:01:42 -0300

    Views login e register

--- tango_with_django_project/rango/views/__init__.py
+++ tango_with_django_project/rango/views/__init__.py
@@ -4,4 +4,5 @@ from .rango_index import RangoIndex
 from .show_category import ShowCategory
 from .add_category import AddCategory
 from .add_page import AddPage
-
+from .register import register
+from .login import user_login
