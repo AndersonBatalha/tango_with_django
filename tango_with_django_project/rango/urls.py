4921b49b76a742e777c077ba193d0e7b2daf1dc0
AndersonBatalha <andersonpbatalha@gmail.com>
2018-10-14 23:03:02 -0300

    Adicionadas as URLs para as views restricted e login

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page', AddPage.as_view(), name='add_page'),
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', views.user_login, name='login'),
+    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^restricted/$', views.restricted, name='restricted'),
 ]
---
+++ tango_with_django_project/rango/views/logout.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Sua conta foi desativada.")
+        else:
+            print("Informações de login inválidas: {0}, {1}".format(username, password))
+            return HttpResponse("Informações de login inválidas foram inseridas.")
+
+    else:
+        return render(request, 'rango/login.html', {})
