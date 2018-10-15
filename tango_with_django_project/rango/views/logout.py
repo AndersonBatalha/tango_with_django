40ab892bb3b496dc11cc3a68948c7c6f82f5268b
AndersonBatalha <andersonpbatalha@gmail.com>
2018-10-14 23:05:10 -0300

    View user_logout

--- tango_with_django_project/rango/views/logout.py
+++ tango_with_django_project/rango/views/logout.py
@@ -1,24 +1,11 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
 
-        user = authenticate(username=username, password=password)
 
-        if user:
-            if user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-            else:
-                return HttpResponse("Sua conta foi desativada.")
-        else:
-            print("Informações de login inválidas: {0}, {1}".format(username, password))
-            return HttpResponse("Informações de login inválidas foram inseridas.")
-
-    else:
-        return render(request, 'rango/login.html', {})
