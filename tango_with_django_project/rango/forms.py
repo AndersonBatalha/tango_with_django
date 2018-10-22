5d4c257384f0a49a5762641eecd9510dee09d33e
AndersonBatalha <andersonpbatalha@gmail.com>
2018-10-22 14:06:01 -0300

    Alterando o label do formulário

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 from .models import Page, Category, UserProfile
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text='Informe a categoria')
-    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    name = forms.CharField(label='Nome', max_length=128, help_text='Informe a categoria')
+    views = forms.IntegerField(label='Views', widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(label='Curtidas', widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     class Meta:
@@ -14,16 +14,16 @@ class CategoryForm(forms.ModelForm):
         fields = ('name',)
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text='Título da página')
-    url = forms.URLField(max_length=200, help_text='URL da página')
-    views = forms.IntegerField(widget=forms.HiddenInput, initial=0)
+    title = forms.CharField(label='Título', max_length=128, help_text='Título da página')
+    url = forms.URLField(label='URL', max_length=200, help_text='URL da página')
+    views = forms.IntegerField(label='Views',widget=forms.HiddenInput, initial=0)
 
     class Meta:
         model = Page
         exclude = ('category',)
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    password = forms.CharField(label='Senha', widget=forms.PasswordInput())
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
