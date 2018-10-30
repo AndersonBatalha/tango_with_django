e149cb664035091a17b94d5d045e36f2c5f53cf4
AndersonBatalha <andersonpbatalha@gmail.com>
2018-10-29 23:44:19 -0300

    finalizando a aparência do template

--- tango_with_django_project/rango/views/add_like.py
+++ tango_with_django_project/rango/views/add_like.py
@@ -7,6 +7,7 @@ def add_like(request, cat_id):
     cat_like = Category.objects.get(pk=cat_id)
 
     cat_like.likes += 1
+    cat_like.views -= 1
     cat_like.save()
 
     return HttpResponseRedirect(reverse('show_category', args=[cat_like.slug]))
