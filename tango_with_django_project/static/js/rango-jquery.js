b113631546f643a5d2bacd7c24aa80669b97a378
AndersonBatalha <andersonpbatalha@gmail.com>
2018-11-07 19:16:35 -0200

    exemplos - JQuery

---
+++ tango_with_django_project/static/js/rango-jquery.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $('#about-btn').click(function (event) {
+        alert('You clicked the button using JQuery!');
+    })
+
+    $('p').hover(function () {
+        $(this).css('color', 'blue');
+    },
+    function () {
+        $(this).css('color', 'red');
+    });
+
+    $('#about-btn').addClass('btn-danger'); // adicionar classes a um elemento através do seu id.
+
+    $('#about-btn').click(function () {
+        msgstr = $('#msg').html();
+        msgstr = msgstr + 'oooo'
+        $('#msg').html(msgstr);
+
+    })
+});
